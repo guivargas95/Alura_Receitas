96d67b8338ff00bc6503baf4878c1f1dcd479f53
guivargas95 <guisouzavargas@outlook.com>
2021-10-05 13:19:48 -0300

    CRUD completo finalizado. Aplicada refatoração dos templates

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,7 @@ def index(request):
     dados = {
         'receitas' : receitas
     }
-    return render(request, 'index.html', dados)
+    return render(request, 'receitas/index.html', dados)
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -16,7 +16,7 @@ def receita(request, receita_id):
     receita_a_exibir = {
         'receita' : receita
     }
-    return render(request, 'receita.html', receita_a_exibir)
+    return render(request, 'receitas/receita.html', receita_a_exibir)
 
 
 def buscar(request):
@@ -34,4 +34,4 @@ def buscar(request):
         'receitas' : lista_receitas
     }
 
-    return render(request, 'buscar.html', dados)
+    return render(request, 'receitas/buscar.html', dados)
